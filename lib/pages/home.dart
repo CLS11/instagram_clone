5db6d999baef3e7ae0cef06fd174f16551618b01
CLS11 <crystall.101600@gmail.com>
2024-11-14 23:56:33 +0530

    Contains all the UI for home page.

---
+++ lib/pages/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/util/bubble_stories.dart';
+import 'package:instagram_clone/util/user_posts.dart';
+
+class UserHome extends StatelessWidget {
+  UserHome({super.key});
+
+  final List<String> people = ['Matt', 'Kris', 'Bob', 'Alex'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Instagram',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Row(
+              children: [
+                Icon(Icons.add),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Icon(Icons.favorite),
+                ),
+                Icon(Icons.share),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 130,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: people.length,
+              itemBuilder: (context, index) {
+                return BubbleStories(
+                  text: people[index],
+                );
+              },
+            ),
+          ),
+          const UserPosts(name: 'Becky'),
+        ],
+      ),
+    );
+  }
+}
