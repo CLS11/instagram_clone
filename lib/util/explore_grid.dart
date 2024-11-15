081708ec963bc943da55740a2f3ea5d18aacf147
CLS11 <crystall.101600@gmail.com>
2024-11-15 12:16:07 +0530

    Explore tab.

---
+++ lib/util/explore_grid.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ExploreGrid extends StatelessWidget {
+  const ExploreGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 20,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(10),
+          child: Container(
+            color: Colors.deepPurple[200],
+          ),
+        );
+      },
+    );
+  }
+}
