9ae2f4a572835483766d2136b8775e14a4ceda7a
CLS11 <crystall.101600@gmail.com>
2024-11-15 12:06:38 +0530

    Search page for displaying all the user feed using GridView builder.

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/util/explore_grid.dart';
 
 class UserSearch extends StatelessWidget {
   const UserSearch({super.key});
@@ -6,7 +7,34 @@ class UserSearch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('SEARCH'),),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            color: Colors.grey,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.grey[400],
+                ),
+                Container(
+                  child: Text(
+                    'Search',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ExploreGrid(),
     );
   }
 }
