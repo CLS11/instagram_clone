db20e9416451d65680fffa7f01cd37e2dcbc9ecf
CLS11 <crystall.101600@gmail.com>
2024-11-15 12:18:08 +0530

    UI for the post displayed on the home screen.

--- lib/util/user_posts.dart
+++ lib/util/user_posts.dart
@@ -7,6 +7,7 @@ class UserPosts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.all(15),
@@ -69,6 +70,52 @@ class UserPosts extends StatelessWidget {
                 Icons.bookmark,
               ),
             ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 15),
+          child: Row(
+            children: [
+              Text(
+                'Liked by',
+              ),
+              Text(
+                'Sevi',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'and',
+              ),
+              Text(
+                'others',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 10, top: 0),
+          child: RichText(
+            text: TextSpan(
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+              children: [
+                TextSpan(
+                  text: name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const TextSpan(
+                  text: 'Hillarious!',
+                ),
+              ],
+            ),
           ),
         ),
       ],
