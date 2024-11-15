c66a55b56ccf55d9cbf52fc6feb292a27cd92a83
CLS11 <crystall.101600@gmail.com>
2024-11-15 12:16:51 +0530

    Shop page.

--- lib/pages/shop.dart
+++ lib/pages/shop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/util/shop_grid.dart';
 
 class UserShop extends StatelessWidget {
   const UserShop({super.key});
@@ -6,8 +7,66 @@ class UserShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('SHOP'),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Shop',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.calendar_today,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(
+                    Icons.menu,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                color: Colors.grey[200],
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.grey[400],
+                    ),
+                    Text(
+                      'Shop',
+                      style: TextStyle(color: Colors.grey[400], fontSize: 15),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const Expanded(
+            child: ShopGrid(),
+          ),
+        ],
       ),
     );
   }
